de2cf7a20c61474007ee3e731030e1201c59af98
Malin Christersson <malinc@malinc.se>
2014-08-14 22:41:14 +0200

    Modda Minecraft med ScriptCraft
    En sammanfattning på svenska.

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<title>Modda Minecraft med ScriptCraft -
+		Installera och kom igång!
+	</title>
+	<meta charset=utf-8 >
+	<link rel="stylesheet" href="minecraft.min.css" type="text/css" />
+</head>
+<body>
+	<header>Modda Minecraft med ScriptCraft</header>
+	<article>
+		<h1>Installera och kom igång!</h1>
+		
+		<p>ScriptCraft är ett plugin till en Minecraft-server. Med ScriptCraft kan du modda Minecraft genom att programmera 
+			i Javascript. ScriptCraft är speciellt gjord för nybörjare och innehåller bland annat en så kallad drönare 
+			(drone på engelska). Drönaren används för att bygga saker i Minecraft och du programmerar själv vad drönaren skall 
+			bygga. </p>	
+		<p>För att använda ScriptCraft måste du först sätta upp en egen server. ScriptCraft är gjord för att fungera med en så
+			kallad Bukkit-server.  </p>
+		<p>Det finns utförliga instruktioner på engelska för installering av både <a href="http://scriptcraftjs.org/">ScriptCraft</a> och 
+			<a href="http://wiki.bukkit.org/Setting_up_a_server">CraftBukkit</a>. Det här en sammanfattning på svenska.  </p>
+		<p>När man skriver kod använder man inte vanliga ordbehandlare (som exempelvis Word) eftersom ordbehandlare formaterar texten. 
+			Det finns speciella utvecklingsmiljöer gjorda för 
+			programmering som ofta är svåra att använda för nybörjare. Det finns också enklare texteditorer som inte är så avancerade men 
+			lättanvända. Här får du förslag på en texteditor som inte är allt 
+			för avancerad men funkar bra för programmering.   </p>
+		<p>För att kunna modda Minecraft måste du ha ett Minecraft-konto. All annan programvara som behövs är gratis.</p>
+			
+			
+		<h2>Ladda ner och installera en texteditor</h2>
+		<h3>Linux</h3>
+		<p>Använd exempelvis <b>gedit</b> vilket är förinstallerat på många Linux-distributioner. </p>
+		<h3>Mac</h3>
+		<p>Installera <b>TextWrangler</b> från App Store.</p>
+		<h3>Windows</h3>
+		<p>Ladda ner och installera <a href="http://notepad-plus-plus.org/">Notepad++</a>. </p>
+		
+		<h2>Sätt upp Bukkit-servern</h2>
+		
+		<p>Om du inte redan har Java installerat så <a href="http://www.java.com/sv/">installera det</a>!</p>
+		
+		<h3>Linux</h3>
+		<ol>
+			<li>
+				<dl>
+					<dt>Hämta serverfilen</dt>
+					<dd>För att köra servern behöver du en serverfil som skall placeras på ett lämpligt ställe.</dd>
+				</dl>
+				<ol>
+					<li>Gör en ny mapp kallad <em>CraftBukkit</em> i din <em>Hem</em>-mapp.</li>
+					<li>Ladda ner den senaste versionen av serverfilen 
+							<a href="http://dl.bukkit.org/latest-rb/craftbukkit.jar">härifrån</a>. Serverfilen heter 
+							<em>craftbukkit-1.6.4-R2.0.jar</em> eller något liknande.</li>
+					<li>Placera den nedladdade filen i den nygjorda mappen <em>CraftBukkit</em>.</li>
+				</ol>
+			</li>
+			<li>
+				<dl>
+					<dt>Gör ett startscript</dt>
+					<dd>För att på ett enkelt sätt kunna starta servern behöver du ett startscript.</dd>
+				</dl>
+				<ol>
+					<li>Öppna gedit (eller en annan texteditor) och klistra in följande kod
+						<pre>#!/bin/sh
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "$BINDIR"
+java -Xmx1024M -jar <span style="color: green;">craftbukkit-1.6.4-R2.0.jar</span> -o true</pre>
+					Om din serverfil inte heter <em>craftbukkit-1.6.4-R2.0.jar</em> så byt ut filnamnet (den gröna texten) till
+					rätt filnamn.
+					</li>
+					<li>Spara filen som <em>craftbukkit.sh</em> i mappen <em>CraftBukkit</em></li>
+					<li>
+						<dl>
+							<dt>Ändra behörigheten på startscriptet så att den blir körbar</dt>
+							<dd>Öppna terminalen och skriv in följande kommando:</dd>
+							<dd><code>chmod +x ~/CraftBukkit/craftbukkit.sh</code></dd>
+						</dl>
+						
+					</li>
+				</ol>
+			</li>
+			<li>
+				<dl>
+					<dt>Starta servern</dt>
+					<dd>Första gången du kör servern skapas de filer som behövs för att konfigurera servern. 
+						Starta servern genom att skriva följande kommando i terminalen:</dd>
+					<dd><code>~/CraftBukkit/craftbukkit.sh</code></dd>
+					<dd>Efter att alla filer skapats, stänger du ner servern genom att skriva <code>stop</code>
+						i terminalfönstret. Stäng <b>alltid</b> ner servern genom att skriva <code>stop</code>.
+						</dd>
+				</dl>
+			</li>
+		</ol>
+		
+		<h3>Mac</h3>
+		<ol>
+			<li>
+				<dl>
+					<dt>Hämta serverfilen</dt>
+					<dd>För att köra servern behöver du en serverfil som skall placeras på ett lämpligt ställe.</dd> 
+				</dl>
+				<ol>
+					<li>Öppna Finder-fönstret och gör en ny mapp kallad <em>CraftBukkit</em> 
+						inuti mappen <em>Dokument</em>.</li>
+					<li>Ladda ner den senaste versionen av serverfilen 
+								<a href="http://dl.bukkit.org/latest-rb/craftbukkit.jar">härifrån</a>. Serverfilen heter 
+								<em>craftbukkit-1.6.4-R2.0.jar</em> eller något liknande.</li>
+					<li>Flytta serverfilen från mappen <em>Hämtade filer</em> till den nygjorda mappen 
+						<em>CraftBukkit</em>.</li>
+				</ol>
+			</li>
+			<li>
+				<dl>
+					<dt>Gör ett startscript</dt>
+					<dd>För att på ett enkelt sätt kunna starta servern behöver du ett startscript.</dd>
+				</dl>
+				<ol>
+					<li>Öppna TextWrangler (eller en annan texteditor) och klistra in följande kod
+						<pre>#!/bin/bash
+cd "$( dirname "$0" )"
+java -Xmx1024M -jar <span style="color: green;">craftbukkit-1.6.4-R2.0.jar</span> -o true</pre>
+					Om din serverfil inte heter <em>craftbukkit-1.6.4-R2.0.jar</em> så byt ut filnamnet (den gröna texten) till
+					rätt filnamn.
+					</li>
+					<li>Spara filen som <em>start_server.command</em> i mappen <em>CraftBukkit</em>.</li>
+					<li>
+						Ändra behörigheten på startscriptet så att den blir körbar 
+						<ul>
+							<li>Öppna programmet Terminal. Du hittar Terminal i mappen <em>Verktygsprogram</em> inuti <em>Program</em>-mappen. </li>
+							<li>När du skall ändra behörigheten på filen <em>start_server.command</em> skall du ange filnamnet och var filen ligger. 
+								Detta kan du göra genom att dra filen <em>start_server.command</em> in i terminal-fönstret, då anges hela 
+								sökvägen till filen.
+								<ul>
+									<li>Börja skriva kommandot <code>chmod a+x </code> i terminalfönstret. </li>
+									<li>Se till att det finns en blank efter kommandot (använd mellanslagstangenten).</li>
+									<li>Klicka <b>inte</b> på returtangenten.</li>
+									<li>Dra in filen <em>start_server.command</em> från Findern in i terminalfönstret.</li>
+									<li>Klicka på returtangenten!</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ol>
+			</li>
+			<li>
+				<dl>
+					<dt>Starta servern</dt>
+					<dd>Första gången du kör servern skapas de filer som behövs för att konfigurera servern. 
+						Starta servern genom att dubbelklicka 
+						på <em>start_server.command</em>. Efter att alla filer skapats, stänger du ner servern genom att skriva <code>stop</code>
+						i terminalfönstret. Stäng <b>alltid</b> ner servern genom att skriva <code>stop</code>.
+						</dd>
+				</dl>
+			</li>
+		</ol>
+		
+		<h3>Windows</h3>
+		<p>Till Windows finns det ett grafiskt användargränssnitt kallat 
+			<a href="http://dev.bukkit.org/bukkit-plugins/bukkitgui/">BukkitGUI</a> (Graphical User Interface).</p>
+		<ol>
+			<li>
+				<dl>
+					<dt>Hämta serverfilen</dt>
+					<dd>För att köra servern behöver du en serverfil som skall placeras på ett lämpligt ställe.</dd> 
+				</dl>
+				<ol>
+					<li>Gör en ny mapp kallad <em>CraftBukkit</em> 
+						inuti mappen <em>Mina Dokument</em>.</li>
+					<li>Ladda ner den senaste versionen av serverfilen 
+								<a href="http://dev.bukkit.org/bukkit-plugins/bukkitgui/">härifrån</a>.</li>
+					<li>Flytta serverfilen till den nygjorda mappen <em>CraftBukkit</em> och dubbelklicka på <em>BukkitGUI.exe</em>.</li>
+				</ol>
+			</li>
+			<li>
+				Starta servern
+				<ol>
+					<li>Välj tabben <em>SuperStart</em> och klicka på <em>Download Latest Recommended Build</em>.</li>
+					<li>Klicka på <em>Launch Server</em>.</li>
+					<li>Om du får felmeddelandet <em>The java path could not be found</em>, gör följande:
+						<ol>
+							<li>Välj <em>Alternative java path</em> efter <em>Java version</em>.</li>
+							<li>Sök efter <em>java.exe</em> och klicka på ok när sökvägen hittats</li>
+						</ol>
+					</li>
+					<li>Efter dessa inställningar kan du kan starta och stänga av servern under tabben <em>General</em>.
+						Klicka på <em>Stop</em> för att stänga ner servern.</li>
+					
+				</ol>
+			</li>
+			
+			
+		</ol>
+		
+		<h2>Konfigurera servern (valfritt)</h2>
+		<p>När du installerat CraftBukkit kan du konfigurera servern.</p>
+		<p> Med Linux och Mac konfigurerar du servern genom att 
+			öppna filen <em>server.properties</em> med den texteditor du laddat ner. Skriv sedan in de ändringar du vill 
+			göra. </p>
+		<p>Med Windows väljer du tabben <em>Server Options</em>.</p> 
+		<p>Här är några exempelkonfigureringar:</p>
+			
+		<pre># en helt platt värld kan vara bra om man vill bygga allt själv
+level-type=FLAT
+generate-structures=false
+
+# creative mode
+gamemode=1
+pvp=false
+
+# inställningar som kan vara bra i klassrumsmiljöer
+online-mode=false
+spawn-npcs=false
+spawn-monsters=false</pre>
+		<p>När du körde servern första gången skapades det en värld. För ett göra en ny värld med den nya konfigureringen måste den
+			gamla världen slängas (eller sparas undan). Släng mappen <em>world</em>, det skapas en ny mapp nästa gång du kör servern. </p>
+
+		<p>För mer information se den engelska 
+			Minecraft-sidan 
+			<a href="http://minecraft.gamepedia.com/Server.properties">server.properties - Minecraft Wiki</a>. </p>
+		
+		<h2>Hämta ScriptCraft</h2>
+		<p>Ladda ner den senaste versionen av ScriptCraft <a href="http://scriptcraftjs.org/download/latest/">härifrån</a>. 
+			Flytta filen <em>Scripcraft.jar</em> till CraftBukkit/plugins.</p>
+			
+		<h2>Testa servern i Minecraft</h2>
+		
+		<ol>
+			<li>Starta Bukkit-servern.</li>
+			<li>
+				<dl>
+					<dt>Gör dig själv till "server operatör"</dt>
+					<dd>Med Linux eller Mac skriver du <code>op <span style="color: green">spelarnamn</span></code>
+						i konsolen. Ordet spelarnamn skall bytas till ditt eget spelarnamn i Minecraft.</dd>
+					<dd>Med Windows väljer du tabben <em>Server Options</em> och lägger till ditt spelarnamn till <em>OPs</em>.</dd>
+				</dl>
+			</li>
+			<li>Starta Minecraft.</li>
+			<li>
+				<dl>
+					<dt>Välj rätt version av Minecraft. </dt>
+					<dd>Den senaste versionen av Minecraft är troligen nyare än den senaste rekommenderade versionen av CraftBukkit.
+						Om CraftBukkit-filen heter <em>craftbukkit-1.6.4-R2.0.jar</em> så är den gjord för version 1.6.4. </dd>
+					<dd>Välj <em>Edit Profile</em> i Minecraft och välj sedan en version som passar CraftBukkit efter <em>Use version</em>.</dd>
+					<dd>Spara profilen.</dd>
+					<dd>Det är också möjligt att ladda ner nyare versioner av CraftBukkit men de nyare versionerna är oftast 
+						beta-versioner. Du hittar beta-versioner av CraftBukkit 
+						<a href="https://dl.bukkit.org/downloads/craftbukkit/list/beta/">här</a>.</dd>
+				</dl>
+			</li>
+			<li>Klicka på Play och välj Multiplayer.</li>
+			<li>Klicka på Add server.</li>
+			<li>Välj ett servernamn vilket som helst och skriv in <code>localhost</code> under Server Address.</li>
+			<li>Börja spela på den nya servern.</li>
+			<li>Klicka på t för att få upp en textrad som man kan skriva på. Skriv in
+				<pre>/js 1+1</pre></li>
+			<li>Om talet 2.0 dyker upp så har du lyckats köra din första Javascript-kod i Minecraft! </li>
+		</ol>
+		
+		<h2>Några enkla byggkommandon</h2>
+		<p>Alla byggblock i Minecraft har ett id-nummer. På sidan <a href="http://minecraft-ids.grahamedgecombe.com/">Minecraft ID List</a> 
+			finns en fullständig lista. Exempelvis har guld id-nummer 41 och röd ull id-nummer 35:14.</p>
+		<p>Prova att skriva in kommandona:</p>
+		<pre>/js box('41')
+/js box('35:14')</pre>
+		<p>Ett rätblock har en bredd, ett djup och en höjd. Gör ett rätblock genom att skriva kommandot:</p>
+		<pre>/js box('41', 3, 5, 10)</pre>
+		<p>Hur gör man ett rätblock av röd ull som är 2 block bred, 100 block hög och 1 block djupt? Testa dig fram!</p>
+		<p>Det finns även kommandon för att göra cylindrar. Prova kommandona:</p>
+		<pre>/js cylinder('41', 3, 5)
+/js cylinder0('41', 5, 3)</pre>
+		<p>Du kan återanvända kommandon som du skrivit. När textraden visas trycker du på pil upp eller pil ned för att se gamla kommandon.
+			Sedan kan du förflytta dig längs ett kommando med hjälp av högerpil och vänsterpil.</p>
+		<p>Hur gör man en ihålig guldcylinder som har radien 10 och höjden 2?</p>
+		<p>Om man vill göra tak till hus kan man använda sig av ett prisma. Prova kommandona:</p>
+		<pre>/js prism('35:14', 6, 10)
+/js prism0('35:14', 15, 5)</pre>
+
+		<h3>Färdiga program</h3>
+		<p>I mappen CraftBukkit/plugins/scriptcraft/plugins/drone/contrib finns exempelprogram som olika användare gjort. Prova 
+			några av programmen genom att skriva:</p>
+		<pre>/js rainbow(30)
+/js amazing(5, 7)
+/js dancefloor(10, 8)</pre>
+		<p>Öppna något av programmen i texteditorn och betrakta koden!</p>
+		<p>Om du vill göra ett längre program själv, måste du skriva det i texteditorn, spara filen, och sedan köra programmet
+			inifrån Minecraft. Se nästa sida <a href="programDrone.php">Programmera drönaren!</a> för mer information.</p>
+	</article>
+	
+</body>
---
+++ programDrone.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<title>Modda Minecraft med ScriptCraft -
+		Programmera drönaren!
+	</title>
+	<meta charset=utf-8 >
+	<link rel="stylesheet" href="minecraft.min.css" type="text/css" />
+</head>
+<body>
+	<header>Modda Minecraft med ScriptCraft</header>
+	<article>
+		<h1>Programmera drönaren!</h1>
+		
+		<p>För information om hur man installerar Bukkit-servern och ScriptCraft se föregående sida 
+			<a href="index.php">Installera och kom igång!</a></p>
+		<p>I ScriptCraft kan man programmera en så kallad drönare (drone på engelska) till att bygga saker i Minecraft. 
+			Här beskrivs hur man programmerar drönaren.</p>
+			
+		<h2>Bygg en husgrund</h2>
+		<p>Börja med att göra en mapp till alla dina program. Mappen bör ligga i CraftBukkit/plugins/Scriptcraft/plugins. 
+			Döp den exempelvis till <em>MinaProgram</em>.</p>
+		<p>Öppna en texteditor och skriv in följande kod:</p>
+		<p><img src="images/code1.png" alt="Kod 1" width="385" height="127"/> </p>
+		<p>Det är viktigt att du skriver koden själv, istället för att bara kopiera in den (därför visas en bild istället för text).</p>
+		<p>Spara filen som <em>mitthus.js</em> i mappen <em>MinaProgram</em>.</p>
+		<p>Kör koden inifrån Minecraft genom att skriva:</p>
+		<pre>/js mitthus()</pre>
+		<p>Prova att göra husgrunden i ett annat material än guld. Välj ett annat ID-nummer än 41 från sidan
+			<a href="http://minecraft-ids.grahamedgecombe.com/">Minecraft ID List</a>. Spara filen efter att du ändrat.
+			För att ändringan skall märkas i Minecraft, måste du köra kommandot: </p>
+		<pre>/js refresh()</pre>
+		<p>innan du kör <code>/js mitthus()</code> igen. Använd piltangenterna för att få fram gamla kommandon i Minecraft.</p>
+			
+		<h3>Vad koden gör</h3>
+		<p>På rad 1 anges var programmet hittar Drone-koden.</p>
+		<p>På rad 3 definieras en funktion döpt till <em>mitthus</em>. Det går bra att ge funktionen ett annat namn. 
+			Efter funktionsnamnet 
+			skall det alltid finnas ett par parenteser <code>()</code>. Därefter skriver man vad funktionen skall göra mellan
+			måsvingarna <code>{}</code>. </p>
+		<p>På rad 4 <b>deklareras</b> ett drönarobjekt <em>d</em>. Det går bra att döpa drönaren till något annat. </p>
+		<p>På rad 5 låter man drönaren använda <b>metoden</b> <em>box</em>.</p>
+		<p>På rad 8 anger man att kommandot <code>/js mitthus()</code> i Minecraft skall <b>anropa</b> funktionen <em>mitthus</em>. Om 
+			du byter rad 8 till: </p>
+		<pre>Drone.extend('hejsan', mitthus);</pre>
+		<p>så kör du programmet från Minecraft genom att skriva <code>/js hejsan()</code>.</p>
+		
+		<h2>Bygg ett hus</h2>
+		<h3>Flytta drönaren</h3>
+		<p>Drönaren är osynlig men befinner sig där du har muspekaren. Det är vid muspekaren huset byggs. För att bygga ett hus av 
+			flera olika bitar måste du kunna flytta på drönaren. Nedan finns en lista
+			på flyttkommandon.  </p>
+		<ul>
+			<li><code>up(antalBlock)</code> - flyttar upp drönaren. Koden <code>up()</code> flyttar drönaren ett block up. 
+				Koden <code>up(5)</code> flyttar drönaren 5 block upp.</li>
+			<li><code>down(antalBlock)</code> - flyttar ner drönaren.</li>
+			<li><code>left(antalBlock)</code> - flyttar drönaren åt vänster.</li>
+			<li><code>right(antalBlock)</code> - flyttar drönaren åt höger.</li>
+			<li><code>fwd(antalBlock)</code> - flyttar drönaren framåt.</li>
+			<li><code>back(antalBlock)</code> - flyttar drönaren bakåt.</li>
+			<li><code>turn(antalVridningar)</code> - vänder drönaren 90 grader åt höger. Koden <code>turn(2)</code> 
+				vänder drönaren två gånger åt höger så att den pekar i rakt motsatt riktning.</li>
+		</ul>
+		
+		<h3>Gör väggar och tak</h3>
+		<p>Ändra koden till:</p>
+		<p><img src="images/code2.png" alt="Kod 2" width="385" height="185"/> </p>
+		<p>Det som står efter <code>//</code> är så kallad kommentarer. Kommentarer är till för den som läser koden och exekveras inte.</p>
+		<p>Provkör programmet i Minecraft!</p>
+		
+		<h3>Gör en variabel</h3>
+		<p>Om du vill ändra storlek på huset så att det är 5*5 block istället för 10*10 block, så måste du gå in och byta ut en siffra 
+			på sex olika ställen i koden. För att göra sådana ändringar enklare, kan du införa en variabel istället för att 
+			använda siffran 10.</p>
+		<p>Ändra koden till:</p>
+		<p><img src="images/code3.png" alt="Kod 3" width="450" height="200"/> </p>
+		<p>Prova att ge variabeln <code>storlek</code> olika värden.</p>
+		
+		<h3>Låt användaren bestämma storleken</h3>
+		<p>Istället för att ändra värdet på variabeln <code>storlek</code> inne i koden, kan du låta användaren ange storleken inifrån
+			Minecraft. Detta gör du genom att ta bort variabeln och istället ge funktionen en så kallad <b>parameter</b> 
+			innanför parenteserna.</p>
+		<p>Ändra koden till:</p>
+		<p><img src="images/code4.png" alt="Kod 4" width="450" height="187"/> </p>
+		<p>Kör koden inifrån Minecraft genom att exempelvis skriva:</p>
+		<pre>/js mitthus(10)</pre>
+		
+		<h3>Kedja ihop byggkommandona</h3>
+		<p>Istället för att skriva varje byggkommando som <code>d.<em>kommando();</em></code> kan man kedja
+		ihop allt byggande med hjälp av punkter till <code>d.<em>kommando().kommando().kommando()</em>;</code>.</p>
+		<p>Koden kan skrivas som:</p>
+		<p><img src="images/code5.png" alt="Kod 5" width="600" height="125"/> </p>
+		<p>där parameternamnet har förkortats till <code>s</code>.</p>
+		<p>Man kan också använda en kedja av kommandon som är uppdelad på flera rader.</p>
+		<p><img src="images/code6.png" alt="Kod 6" width="370" height="198"/> </p>
+		
+		<h2>Skriv en villkorssats och ge ett felmeddelande</h2>
+		<p>När man låter användare ange parametervärden är det en god idé att kontrollera vad användaren faktiskt matat in, 
+			och eventuellt ge ett felmeddelande. Det kan exempelvis fungera så här: </p>
+		<div style="width: 30em; margin: auto">
+			Om användaren angett en storlek mindre än 3 <br />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ge ett felmeddelande <br />
+			annars <br />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bygg huset
+		</div>
+		<p>En sådan villkorssats kan du skriva med hjälp av en så kallad if-else-sats.</p>
+		<p>Felmeddelandet kan visas på en Minecraft-skylt. Drönaren kan göra en skylt med hjälp
+			av metoden <code>sign</code>.</p>
+		<p>Ändra koden till:</p>
+		<p><img src="images/code7.png" alt="Kod 7" width="633" height="186"/> </p>
+		<p>Provkör:</p>
+		<pre>/js mitthus(2)</pre>
+		<p>För mer information (på engelska) om skyltar se 
+			<a href="https://github.com/walterhiggins/ScriptCraft/blob/master/docs/API-Reference.md#dronesign-method">Drone.sign() method</a>.</p>
+		
+		<h2>Skriv en loop</h2>
+		<p>Om man vill göra ett trevåningshus, skulle man kunna använda följande kod:</p>
+		<p><img src="images/code8.png" alt="Kod 8" width="630" height="260"/> </p>
+		<p>Istället för att skriva samma två rader kod tre gånger, kan man göra en loop. Ett sätt att göra en loop
+			är att skriva en så kallad for-sats. En for-sats som utför samma sak tre gånger kan se ut som i koden nedan:</p>
+		<p><img src="images/code9.png" alt="Kod 9" width="657" height="230"/> </p>
+		<p>Raderna 9 och 10 körs tre gånger. Först gången är variabeln i=0, andra gången är i=1, tredje gången är i=2; därefter avslutas loopen 
+			eftersom i inte längre är mindre än 3.</p>
+		<p>Du kan också införa ännu en parameter för att låta användaren ange antal våningar. I koden nedan kallas denna parameter 
+			för <code>n</code> och for-satsen utförs <code>n</code>gånger.</p>
+		<p><img src="images/code10.png" alt="Kod 10" width="657" height="230"/> </p>
+		<p>Provkör koden inifrån Minecraft:</p>	
+		<pre>/js mitthus(20, 4)</pre>
+		
+		<h2>Gör egna hus,</h2>
+		<p>eller tunnlar, eller järnvägar, eller villa-kvarter, eller...</p>
+		<p>För med information (på engelska) om drönaren, se 
+			<a href="https://github.com/walterhiggins/ScriptCraft/blob/master/docs/API-Reference.md#drone-plugin">ScriptCraft/API - Drone Plugin</a>. </p>
+		
+	</article>
+	
+</body>
